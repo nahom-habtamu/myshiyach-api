4f2c5956ff5160e7c3529c3184ca04525fcddd9f
Nahom Habtamu <nahomhab2626@gmail.com>
2022-05-10 11:00:37 +0300

    Configure Mongo and Add Some Configurations in package.json to work correctly with heroku

--- utils/connectToMongo.js
+++ utils/connectToMongo.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
-module.exports = async function(dbName){
+module.exports = function (dbName) {
     try {
-        await mongoose.connect(`mongodb://localhost/${dbName}`, {
-            useNewUrlParser : true,
-            useUnifiedTopology : true,
+        mongoose.connect(`mongodb+srv://nahom-hab:ecommercetest@cluster0.rxw0t.mongodb.net/${dbName}?retryWrites=true&w=majority`, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
         });
-        console.log("Connected To { " + dbName + " } database succesfully"); 
-    } 
+        console.log("Connected To { " + dbName + " } database succesfully");
+    }
     catch (error) {
         console.log(error.message);
     }
