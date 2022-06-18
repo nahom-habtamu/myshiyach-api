3cec3ac4361d05a3fd090035676c965e23aaaebe
Nahom Habtamu <nahomhab2626@gmail.com>
2022-06-18 13:51:08 +0300

    Allow Empty To User Email

--- validation-schemas/UserRequestValidationSchema.js
+++ validation-schemas/UserRequestValidationSchema.js
@@ -9,18 +9,18 @@ const createUserRequestValidationSchema = Joi.object({
     fullName: Joi.string().required().max(50).min(5),
     password: Joi.string().required().max(255).min(6),
     phoneNumber: Joi.string().required(),
-    email: Joi.string().min(0),
+    email: Joi.string().allow(""),
 });
 
 const putUserRequestValidationSchema = Joi.object({
     fullName: Joi.string().required().max(50).min(5),
-    email: Joi.string(),
+    email: Joi.string().allow(""),
     phoneNumber: Joi.string().required(),
 });
 
 const patchUserRequestValidationSchema = Joi.object({
     fullName: Joi.string().max(50).min(5),
-    email: Joi.string().max(50).min(5),
+    email: Joi.string().allow(""),
     phoneNumber: Joi.string(),
 });
 
