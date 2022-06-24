2dad3abfee54d34f4c8b53bf3212c026430afd7f
Nahom Habtamu <nahomhab2626@gmail.com>
2022-06-24 11:28:40 +0300

    Remove AdditionalData for subCategories

--- validation-schemas/SubCategoryRequestValidationSchema.js
+++ validation-schemas/SubCategoryRequestValidationSchema.js
@@ -2,17 +2,14 @@ const Joi = require('joi');
 
 const createSubCategoryRequestValidationSchema = Joi.object({
     title: Joi.string().required(),
-    additionalData: Joi.array().items(Joi.string())
 });
 
 const putSubCategoryRequestValidationSchema = Joi.object({
     title: Joi.string().required(),
-    additionalData: Joi.array().items(Joi.string())
 });
 
 const patchSubCategoryRequestValidationSchema = Joi.object({
     title: Joi.string(),
-    additionalData: Joi.array().items(Joi.string())
 })
 
 module.exports = {
